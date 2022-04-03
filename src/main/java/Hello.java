98898fa3df833e04822d0dcc1e3677c13202c085
chendaoqiang <chendaoqiang1024@qq.com>
2022-04-03 21:52:55 +0800

    第一次修改

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello,github:)");
+        System.out.println("hello,github:) add content");
     }
 }
